651b9fa51a0fd4e0418df587db2bcf2ce05fbf5b
aamir814154 <amiralam814154@gmail.com>
2025-11-20 00:52:34 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println"AAMIR KHAN KI JAI HOOOOOOOO  IMRAN";
+        Systemoutprintln"AAMIR KHAN KI JAI HOOOOOOOO  IMRAN";
     }
 }
